23ac4186b41e91da1358dfaa5abd6a628ab15f9a
Inan Evin <inanevin@gmail.com>
2018-12-14 17:40:32 +0300

    Test

--- Course Data Analyzer/src/course/data/analyzer/CourseDataAnalyzer.java
+++ Course Data Analyzer/src/course/data/analyzer/CourseDataAnalyzer.java
@@ -16,7 +16,7 @@ public class CourseDataAnalyzer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        //
         ///
     }
     
